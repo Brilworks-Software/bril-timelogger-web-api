e6b1388c6f08f226d9f7e07e00c7684134236bf6
nayansukhadiya31 <nayan.s@brilworks.com>
2026-01-06 14:30:05 +0530

    added middleware for the api

---
+++ app/api/_middleware.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+}
